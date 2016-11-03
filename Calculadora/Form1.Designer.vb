359d72f8da52999c280890b2d9bd010bfc74a377
Vulcano <Vulcano@DESKTOP-HKPUO45>
2016-11-03 08:24:26 +0100

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ButtonEqual = New System.Windows.Forms.Button()
+        Me.Num0 = New System.Windows.Forms.Button()
+        Me.ButtonMult = New System.Windows.Forms.Button()
+        Me.ButtonDiv = New System.Windows.Forms.Button()
+        Me.Num3 = New System.Windows.Forms.Button()
+        Me.Num2 = New System.Windows.Forms.Button()
+        Me.ButtonMen = New System.Windows.Forms.Button()
+        Me.Num1 = New System.Windows.Forms.Button()
+        Me.inputCalc = New System.Windows.Forms.TextBox()
+        Me.Buttonmas = New System.Windows.Forms.Button()
+        Me.negativ = New System.Windows.Forms.Button()
+        Me.Buttoncoma = New System.Windows.Forms.Button()
+        Me.ButtonAC = New System.Windows.Forms.Button()
+        Me.Num9 = New System.Windows.Forms.Button()
+        Me.Num8 = New System.Windows.Forms.Button()
+        Me.Num7 = New System.Windows.Forms.Button()
+        Me.Num6 = New System.Windows.Forms.Button()
+        Me.Num5 = New System.Windows.Forms.Button()
+        Me.Num4 = New System.Windows.Forms.Button()
+        Me.oper = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ButtonEqual
+        '
+        Me.ButtonEqual.Location = New System.Drawing.Point(93, 311)
+        Me.ButtonEqual.Name = "ButtonEqual"
+        Me.ButtonEqual.Size = New System.Drawing.Size(151, 35)
+        Me.ButtonEqual.TabIndex = 0
+        Me.ButtonEqual.Text = "="
+        Me.ButtonEqual.UseVisualStyleBackColor = True
+        '
+        'Num0
+        '
+        Me.Num0.Location = New System.Drawing.Point(39, 311)
+        Me.Num0.Name = "Num0"
+        Me.Num0.Size = New System.Drawing.Size(39, 35)
+        Me.Num0.TabIndex = 17
+        Me.Num0.TabStop = False
+        Me.Num0.Text = "0"
+        Me.Num0.UseVisualStyleBackColor = True
+        '
+        'ButtonMult
+        '
+        Me.ButtonMult.Location = New System.Drawing.Point(205, 205)
+        Me.ButtonMult.Name = "ButtonMult"
+        Me.ButtonMult.Size = New System.Drawing.Size(39, 35)
+        Me.ButtonMult.TabIndex = 12
+        Me.ButtonMult.TabStop = False
+        Me.ButtonMult.Text = "*"
+        Me.ButtonMult.UseVisualStyleBackColor = True
+        '
+        'ButtonDiv
+        '
+        Me.ButtonDiv.Location = New System.Drawing.Point(205, 262)
+        Me.ButtonDiv.Name = "ButtonDiv"
+        Me.ButtonDiv.Size = New System.Drawing.Size(39, 35)
+        Me.ButtonDiv.TabIndex = 16
+        Me.ButtonDiv.TabStop = False
+        Me.ButtonDiv.Text = "/"
+        Me.ButtonDiv.UseVisualStyleBackColor = True
+        '
+        'Num3
+        '
+        Me.Num3.Location = New System.Drawing.Point(151, 262)
+        Me.Num3.Name = "Num3"
+        Me.Num3.Size = New System.Drawing.Size(39, 35)
+        Me.Num3.TabIndex = 15
+        Me.Num3.TabStop = False
+        Me.Num3.Text = "3"
+        Me.Num3.UseVisualStyleBackColor = True
+        '
+        'Num2
+        '
+        Me.Num2.Location = New System.Drawing.Point(93, 262)
+        Me.Num2.Name = "Num2"
+        Me.Num2.Size = New System.Drawing.Size(39, 35)
+        Me.Num2.TabIndex = 14
+        Me.Num2.TabStop = False
+        Me.Num2.Text = "2"
+        Me.Num2.UseVisualStyleBackColor = True
+        '
+        'ButtonMen
+        '
+        Me.ButtonMen.Location = New System.Drawing.Point(205, 148)
+        Me.ButtonMen.Name = "ButtonMen"
+        Me.ButtonMen.Size = New System.Drawing.Size(39, 35)
+        Me.ButtonMen.TabIndex = 8
+        Me.ButtonMen.TabStop = False
+        Me.ButtonMen.Text = "-"
+        Me.ButtonMen.UseVisualStyleBackColor = True
+        '
+        'Num1
+        '
+        Me.Num1.Location = New System.Drawing.Point(39, 262)
+        Me.Num1.Name = "Num1"
+        Me.Num1.Size = New System.Drawing.Size(39, 35)
+        Me.Num1.TabIndex = 13
+        Me.Num1.TabStop = False
+        Me.Num1.Text = "1"
+        Me.Num1.UseVisualStyleBackColor = True
+        '
+        'inputCalc
+        '
+        Me.inputCalc.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.inputCalc.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.inputCalc.Enabled = False
+        Me.inputCalc.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.inputCalc.Location = New System.Drawing.Point(39, 40)
+        Me.inputCalc.Name = "inputCalc"
+        Me.inputCalc.ReadOnly = True
+        Me.inputCalc.Size = New System.Drawing.Size(205, 19)
+        Me.inputCalc.TabIndex = 3
+        Me.inputCalc.TabStop = False
+        Me.inputCalc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Buttonmas
+        '
+        Me.Buttonmas.Location = New System.Drawing.Point(205, 96)
+        Me.Buttonmas.Name = "Buttonmas"
+        Me.Buttonmas.Size = New System.Drawing.Size(39, 35)
+        Me.Buttonmas.TabIndex = 4
+        Me.Buttonmas.TabStop = False
+        Me.Buttonmas.Text = "+"
+        Me.Buttonmas.UseVisualStyleBackColor = True
+        '
+        'negativ
+        '
+        Me.negativ.Location = New System.Drawing.Point(151, 96)
+        Me.negativ.Name = "negativ"
+        Me.negativ.Size = New System.Drawing.Size(39, 35)
+        Me.negativ.TabIndex = 3
+        Me.negativ.TabStop = False
+        Me.negativ.Text = "+/-"
+        Me.negativ.UseVisualStyleBackColor = True
+        '
+        'Buttoncoma
+        '
+        Me.Buttoncoma.Location = New System.Drawing.Point(93, 96)
+        Me.Buttoncoma.Name = "Buttoncoma"
+        Me.Buttoncoma.Size = New System.Drawing.Size(39, 35)
+        Me.Buttoncoma.TabIndex = 2
+        Me.Buttoncoma.TabStop = False
+        Me.Buttoncoma.Text = ","
+        Me.Buttoncoma.UseVisualStyleBackColor = True
+        '
+        'ButtonAC
+        '
+        Me.ButtonAC.Location = New System.Drawing.Point(39, 96)
+        Me.ButtonAC.Name = "ButtonAC"
+        Me.ButtonAC.Size = New System.Drawing.Size(39, 35)
+        Me.ButtonAC.TabIndex = 1
+        Me.ButtonAC.TabStop = False
+        Me.ButtonAC.Text = "AC"
+        Me.ButtonAC.UseVisualStyleBackColor = True
+        '
+        'Num9
+        '
+        Me.Num9.Location = New System.Drawing.Point(151, 148)
+        Me.Num9.Name = "Num9"
+        Me.Num9.Size = New System.Drawing.Size(39, 35)
+        Me.Num9.TabIndex = 7
+        Me.Num9.TabStop = False
+        Me.Num9.Text = "9"
+        Me.Num9.UseVisualStyleBackColor = True
+        '
+        'Num8
+        '
+        Me.Num8.Location = New System.Drawing.Point(93, 148)
+        Me.Num8.Name = "Num8"
+        Me.Num8.Size = New System.Drawing.Size(39, 35)
+        Me.Num8.TabIndex = 6
+        Me.Num8.TabStop = False
+        Me.Num8.Text = "8"
+        Me.Num8.UseVisualStyleBackColor = True
+        '
+        'Num7
+        '
+        Me.Num7.Location = New System.Drawing.Point(39, 148)
+        Me.Num7.Name = "Num7"
+        Me.Num7.Size = New System.Drawing.Size(39, 35)
+        Me.Num7.TabIndex = 5
+        Me.Num7.TabStop = False
+        Me.Num7.Text = "7"
+        Me.Num7.UseVisualStyleBackColor = True
+        '
+        'Num6
+        '
+        Me.Num6.Location = New System.Drawing.Point(151, 205)
+        Me.Num6.Name = "Num6"
+        Me.Num6.Size = New System.Drawing.Size(39, 35)
+        Me.Num6.TabIndex = 11
+        Me.Num6.TabStop = False
+        Me.Num6.Text = "6"
+        Me.Num6.UseVisualStyleBackColor = True
+        '
+        'Num5
+        '
+        Me.Num5.Location = New System.Drawing.Point(93, 205)
+        Me.Num5.Name = "Num5"
+        Me.Num5.Size = New System.Drawing.Size(39, 35)
+        Me.Num5.TabIndex = 10
+        Me.Num5.TabStop = False
+        Me.Num5.Text = "5"
+        Me.Num5.UseVisualStyleBackColor = True
+        '
+        'Num4
+        '
+        Me.Num4.Location = New System.Drawing.Point(39, 205)
+        Me.Num4.Name = "Num4"
+        Me.Num4.Size = New System.Drawing.Size(39, 35)
+        Me.Num4.TabIndex = 9
+        Me.Num4.TabStop = False
+        Me.Num4.Text = "4"
+        Me.Num4.UseVisualStyleBackColor = True
+        '
+        'oper
+        '
+        Me.oper.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.oper.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.oper.Enabled = False
+        Me.oper.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.oper.Location = New System.Drawing.Point(39, 25)
+        Me.oper.Name = "oper"
+        Me.oper.ReadOnly = True
+        Me.oper.Size = New System.Drawing.Size(205, 15)
+        Me.oper.TabIndex = 3
+        Me.oper.TabStop = False
+        Me.oper.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.ButtonEqual
+        Me.AccessibleRole = System.Windows.Forms.AccessibleRole.Window
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.Disable
+        Me.ClientSize = New System.Drawing.Size(293, 378)
+        Me.Controls.Add(Me.Num6)
+        Me.Controls.Add(Me.Num5)
+        Me.Controls.Add(Me.Num4)
+        Me.Controls.Add(Me.Num9)
+        Me.Controls.Add(Me.Num8)
+        Me.Controls.Add(Me.Num7)
+        Me.Controls.Add(Me.Buttonmas)
+        Me.Controls.Add(Me.negativ)
+        Me.Controls.Add(Me.Buttoncoma)
+        Me.Controls.Add(Me.ButtonAC)
+        Me.Controls.Add(Me.ButtonEqual)
+        Me.Controls.Add(Me.Num0)
+        Me.Controls.Add(Me.ButtonMult)
+        Me.Controls.Add(Me.ButtonDiv)
+        Me.Controls.Add(Me.Num3)
+        Me.Controls.Add(Me.Num2)
+        Me.Controls.Add(Me.ButtonMen)
+        Me.Controls.Add(Me.Num1)
+        Me.Controls.Add(Me.oper)
+        Me.Controls.Add(Me.inputCalc)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonEqual As Button
+    Friend WithEvents Num0 As Button
+    Friend WithEvents ButtonMult As Button
+    Friend WithEvents ButtonDiv As Button
+    Friend WithEvents Num3 As Button
+    Friend WithEvents Num2 As Button
+    Friend WithEvents ButtonMen As Button
+    Friend WithEvents Num1 As Button
+    Friend WithEvents inputCalc As TextBox
+    Friend WithEvents Buttonmas As Button
+    Friend WithEvents negativ As Button
+    Friend WithEvents Buttoncoma As Button
+    Friend WithEvents ButtonAC As Button
+    Friend WithEvents Num9 As Button
+    Friend WithEvents Num8 As Button
+    Friend WithEvents Num7 As Button
+    Friend WithEvents Num6 As Button
+    Friend WithEvents Num5 As Button
+    Friend WithEvents Num4 As Button
+    Friend WithEvents oper As TextBox
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,332 @@
+﻿
+Public Class Form1
+    Dim contador As Integer = 0
+    Dim valor1 As Double
+        Dim valor2 As Double
+        Dim Resu As Double
+        Dim symbol As String
+        Dim view As String
+        Dim comaComp As Boolean = False
+    Dim positComp As Boolean = False
+    Dim operadorPendiente As String = Nothing
+    Dim pulsoOperador As Boolean = Nothing
+    Dim comparadorSigno As String
+
+    'Aquí van todos los eventos del teclado
+    Private Sub ListBox1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+
+            ' Si se pulsa un numero se inserta
+            If e.KeyCode = Keys.NumPad1 Or e.KeyCode = Keys.D1 Then
+                Agregar("1")
+            End If
+
+            If e.KeyCode = Keys.NumPad2 Or e.KeyCode = Keys.D2 Then
+                Agregar("2")
+            End If
+
+            If e.KeyCode = Keys.NumPad3 Or e.KeyCode = Keys.D3 Then
+                Agregar("3")
+            End If
+
+            If e.KeyCode = Keys.NumPad4 Or e.KeyCode = Keys.D4 Then
+                Agregar("4")
+            End If
+
+            If e.KeyCode = Keys.NumPad5 Or e.KeyCode = Keys.D5 Then
+                Agregar("5")
+            End If
+
+            If e.KeyCode = Keys.NumPad6 Or e.KeyCode = Keys.D6 Then
+                Agregar("6")
+            End If
+
+            If e.KeyCode = Keys.NumPad7 Or e.KeyCode = Keys.D7 Then
+                Agregar("7")
+            End If
+
+            If e.KeyCode = Keys.NumPad8 Or e.KeyCode = Keys.D8 Then
+                Agregar("8")
+            End If
+
+            If e.KeyCode = Keys.NumPad9 Or e.KeyCode = Keys.D9 Then
+                Agregar("9")
+            End If
+            If e.KeyData = (Keys.Shift + Keys.D0) Then
+
+                ButtonEqual.Select()
+                accionEqual()
+
+            Else
+                If e.KeyCode = Keys.NumPad0 Or e.KeyCode = Keys.D0 Then
+                    Agregar("0")
+                End If
+            End If
+
+
+
+        'Teclas de operadores
+        If e.KeyCode = Keys.Divide Then
+            accion("divi", "/")
+        End If
+
+            If e.KeyCode = Keys.Multiply Then
+            accion("multi", "*")
+        End If
+
+            If e.KeyCode = Keys.Subtract Then
+            accion("resta", "-")
+        End If
+
+            If e.KeyCode = Keys.Add Then
+            accion("suma", "+")
+        End If
+
+        If e.KeyCode = Keys.Delete Then
+            accionAC()
+        End If
+
+        If e.KeyCode = Keys.Delete Then
+                accionAC()
+            End If
+
+        If e.KeyCode = Keys.Decimal Then
+            Buttoncoma.PerformClick()
+        End If
+        If e.KeyCode = Keys.Return Then
+            ButtonEqual.Select()
+            accionEqual()
+
+        End If
+
+        If e.KeyCode = Keys.Back Then
+            accionRetroceso()
+        End If
+
+    End Sub
+
+
+
+        '**************************************************************************
+        ' Eventos de clic del raton
+        '**************************************************************************
+
+        Private Sub Num1_Click(sender As Object, e As EventArgs) Handles Num1.Click
+            Agregar("1")
+        End Sub
+
+        Private Sub Num2_Click(sender As Object, e As EventArgs) Handles Num2.Click
+            Agregar("2")
+        End Sub
+
+        Private Sub Num3_Click(sender As Object, e As EventArgs) Handles Num3.Click
+            Agregar("3")
+        End Sub
+        Private Sub Num4_Click(sender As Object, e As EventArgs) Handles Num4.Click
+            Agregar("4")
+        End Sub
+        Private Sub Num5_Click(sender As Object, e As EventArgs) Handles Num5.Click
+            Agregar("5")
+        End Sub
+        Private Sub Num6_Click(sender As Object, e As EventArgs) Handles Num6.Click
+            Agregar("6")
+        End Sub
+        Private Sub Num7_Click(sender As Object, e As EventArgs) Handles Num7.Click
+            Agregar("7")
+        End Sub
+        Private Sub Num8_Click(sender As Object, e As EventArgs) Handles Num8.Click
+            Agregar("8")
+        End Sub
+        Private Sub Num9_Click(sender As Object, e As EventArgs) Handles Num9.Click
+            Agregar("9")
+        End Sub
+        Private Sub Num0_Click(sender As Object, e As EventArgs) Handles Num0.Click
+            Agregar("0")
+        End Sub
+
+        Private Sub ButtonAC_Click(sender As Object, e As EventArgs) Handles ButtonAC.Click
+            accionAC()
+        End Sub
+
+        Private Sub Buttonmas_Click(sender As Object, e As EventArgs) Handles Buttonmas.Click
+        accion("suma", "+")
+    End Sub
+
+    Private Sub ButtonMen_Click(sender As Object, e As EventArgs) Handles ButtonMen.Click
+        accion("resta", "-")
+    End Sub
+
+    Private Sub ButtonMult_Click(sender As Object, e As EventArgs) Handles ButtonMult.Click
+        accion("multi", "*")
+    End Sub
+
+        Private Sub ButtonDiv_Click(sender As Object, e As EventArgs) Handles ButtonDiv.Click
+        accion("divi", "/")
+    End Sub
+
+        Private Sub ButtonEqual_Click(sender As Object, e As EventArgs) Handles ButtonEqual.Click
+        accionEqual()
+
+    End Sub
+
+    Private Sub Buttoncoma_Click(sender As Object, e As EventArgs) Handles Buttoncoma.Click
+        accionComa()
+    End Sub
+
+
+
+    '*************************************************
+
+    'Declaración de acciones
+
+    '*************************************************
+
+    Private Sub accionAC()
+            valor1 = Nothing
+            valor2 = Nothing
+        inputCalc.Text() = "0"
+        oper.Text() = ""
+            view = ""
+            operadorPendiente = ""
+        contador = 0
+        comaComp = False
+        positComp = False
+    End Sub
+
+    Private Sub accion(v1 As String, v2 As String)
+        comaComp = False
+        If v1 IsNot "" And comparadorSigno IsNot v1 And oper.Text IsNot "" Then
+
+            view = view.Remove(oper.Text.Length - 2)
+            view = view & v2 & " "
+            operadorPendiente = ""
+            operar(v1)
+            symbol = v2
+            oper.Text = view
+        End If
+
+        If contador = 0 Then
+            If (inputCalc.Text IsNot "") Then
+
+                view = view & inputCalc.Text & " " & v2 & " "
+                operar(v1)
+                inputCalc.Text = ""
+                symbol = v2
+                oper.Text = view
+                contador = 1
+                If valor1 = Nothing Then
+                    valor1 = valor2
+                    inputCalc.Text = valor2
+                Else
+                    inputCalc.Text = valor1
+                End If
+            Else
+                contador = 0
+            End If
+        End If
+        comparadorSigno = v1
+    End Sub
+
+
+    Private Sub accionComa()
+        If (comaComp = False) Then
+            If (inputCalc.Text = "") Then
+                inputCalc.Text() = inputCalc.Text() & "0,"
+                comaComp = True
+            Else
+                inputCalc.Text() = inputCalc.Text() & ","
+                comaComp = True
+            End If
+        End If
+    End Sub
+
+
+    Private Sub accionEqual()
+
+        If (inputCalc.Text IsNot "") Then
+
+            valor2 = inputCalc.Text()
+            If (operadorPendiente = "suma") Then
+                valor1 = valor1 + valor2
+            End If
+            If (operadorPendiente = "resta") Then
+                valor1 = valor1 - valor2
+            End If
+            If (operadorPendiente = "multi") Then
+                valor1 = valor1 * valor2
+            End If
+            If (operadorPendiente = "divi") Then
+                valor1 = valor1 / valor2
+            End If
+            operadorPendiente = ""
+            inputCalc.Text() = valor1
+            view = ""
+            oper.Text() = ""
+            End If
+
+    End Sub
+    Private Sub accionRetroceso()
+        If inputCalc.Text IsNot "" Then
+            oper.Text() = oper.Text.Remove(oper.Text.Length - 1)
+            inputCalc.Text = inputCalc.Text.Remove(inputCalc.Text.Length - 1)
+        End If
+    End Sub
+
+    Function operar(operador As String) As Double
+
+            valor2 = inputCalc.Text
+            pulsoOperador = True
+            Select Case operadorPendiente
+                Case "suma"
+                    valor1 = valor1 + valor2
+                Case "resta"
+                    valor1 = valor1 - valor2
+                Case "multi"
+                    valor1 = valor1 * valor2
+                Case "divi"
+                    valor1 = valor1 / valor2
+                Case "1ª"
+                    valor1 = valor2
+            End Select
+            operadorPendiente = operador
+
+        End Function
+
+    Private Sub Agregar(numero As String)
+        If pulsoOperador Then
+            inputCalc.Text = ""
+            pulsoOperador = False
+        End If
+        'Si la cadena empieza por 0,
+        If InStr(1, inputCalc.Text, "0,") Then
+            inputCalc.Text() = inputCalc.Text() & numero
+
+            'Si la cadena empieza por 0
+        ElseIf MID(inputCalc.Text, 1, 1) = "0" Then
+            inputCalc.Text = ""
+            inputCalc.Text() = inputCalc.Text() & numero
+        Else
+            inputCalc.Text() = inputCalc.Text() & numero
+        End If
+
+        contador = 0
+        ButtonEqual.Select()
+    End Sub
+
+    Private Sub negativ_Click(sender As Object, e As EventArgs) Handles negativ.Click
+        If (positComp = False) Then
+            inputCalc.Text() = "-" & inputCalc.Text()
+
+            positComp = True
+        Else
+            inputCalc.Text() = inputCalc.Text().TrimStart("-")
+            view = view
+            oper.Text = oper.Text.TrimStart("-")
+            positComp = False
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        operadorPendiente = "1ª"
+        inputCalc.Text = "0"
+    End Sub
+    End Class
